c37f7ee88c630184559b252d9d2a78ec9a59b976
mario <mario@LENNY>
2022-06-26 20:58:41 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio clase 13/Model/Billetera.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_clase_13.Model
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public decimal Total()
+        {
+            var total = BilletesDe10 * 10 +
+                        BilletesDe20 * 20 +
+                        BilletesDe50 * 50 +
+                        BilletesDe100 * 100 +
+                        BilletesDe200 * 200 +
+                        BilletesDe500 * 500 +
+                        BilletesDe1000 * 1000;
+            return total;
+        }
+
+        public Billetera Combinar (Billetera segundaBilletera)
+        {
+            var nuevaBilletera = new Billetera();
+
+            //COMBINAMOS LAS DOS BILLETERAS
+
+            nuevaBilletera.BilletesDe10 = BilletesDe10 + segundaBilletera.BilletesDe10;
+            nuevaBilletera.BilletesDe20 = BilletesDe20 + segundaBilletera.BilletesDe20;
+            nuevaBilletera.BilletesDe50 = BilletesDe50 + segundaBilletera.BilletesDe50;
+            nuevaBilletera.BilletesDe100 = BilletesDe100 + segundaBilletera.BilletesDe100;
+            nuevaBilletera.BilletesDe200 = BilletesDe200 + segundaBilletera.BilletesDe200;
+            nuevaBilletera.BilletesDe500 = BilletesDe500 + segundaBilletera.BilletesDe500;
+            nuevaBilletera.BilletesDe1000 = BilletesDe1000 + segundaBilletera.BilletesDe1000;
+
+            //VACIAMOS LA PRIMER BILLETERA
+
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe200 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+
+            //VACIAMOS LA SEGUNDA BILLETERA
+
+            segundaBilletera.BilletesDe10 = 0;
+            segundaBilletera.BilletesDe20 = 0;
+            segundaBilletera.BilletesDe50 = 0;
+            segundaBilletera.BilletesDe100 = 0;
+            segundaBilletera.BilletesDe200 = 0;
+            segundaBilletera.BilletesDe500 = 0;
+            segundaBilletera.BilletesDe1000 = 0;
+
+            return nuevaBilletera;
+            
+
+        }
+    }
+}
---
+++ Ejercicio clase 13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Ejercicio_clase_13.Model;
+
+var Billetera1 = new Billetera()
+{
+    BilletesDe100 = 5,
+    BilletesDe20 = 6,
+    BilletesDe10 = 2,
+    BilletesDe1000 = 1,
+    BilletesDe200 = 2,
+    BilletesDe50 = 4,
+    BilletesDe500 = 1
+    
+};
+
+var Billetera2 = new Billetera()
+{
+    BilletesDe10 = 1,
+    BilletesDe20 = 3,
+    BilletesDe50 = 1,
+    BilletesDe100 = 5,
+    BilletesDe200 = 1,
+    BilletesDe500 = 1,
+    BilletesDe1000 = 5
+    
+};
+
+Console.WriteLine($"Billetera 1 tiene {Billetera1.Total()} pesos");
+Console.WriteLine($"Billetera 2 tiene {Billetera2.Total()} pesos");
+
+var Billetera3 = Billetera1.Combinar(Billetera2);
+
+Console.WriteLine($"La nueva Billetera tiene {Billetera3.Total()} pesos");
+
+Console.WriteLine($"billetera 1 quedó en {Billetera1.Total()}");
+Console.WriteLine($"billetera 2 quedó en {Billetera2.Total()}");
+
